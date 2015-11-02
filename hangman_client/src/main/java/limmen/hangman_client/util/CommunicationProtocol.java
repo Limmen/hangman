c7c5c2a3cad807b570138c28f3aa6b3a3bda55e2
limmen <kimham@kth.se>
2015-11-02 17:58:44 +0100

    Initial commit

---
+++ hangman_client/src/main/java/limmen/hangman_client/util/CommunicationProtocol.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package limmen.hangman_client.util;
+
+/**
+ *
+ * @author kim
+ */
+public interface CommunicationProtocol {
+    
+}
